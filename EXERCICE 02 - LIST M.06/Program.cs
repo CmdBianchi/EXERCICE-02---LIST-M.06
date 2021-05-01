9ac64ba87a79dd03edf4026941449e3f0dc24b95
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-05-01 14:48:04 -0300

    Adicionar arquivos de projeto.

---
+++ EXERCICE 02 - LIST M.06/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+namespace EXERCICE_02___LIST_M._06 {
+    //-----> INICIO
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    class Program {
+        static void Main(string[] args) {
+            List<string> list = new List<string>(); //-------------- > DECLARAÇÃO DA LISTA 
+
+            list.Add("Maria");
+            list.Add("Alex");
+            list.Add("Bob");
+            list.Add("Anna");
+            list.Insert(2, "Marco");
+
+            foreach (string obj in list) { //-------------- > PRINTA OS OBJETOS DA LISTA
+                Console.WriteLine(obj);
+            }
+            Console.Write("list count: " + list.Count);
+
+            string s1 = list.Find( x => x[0] =='A'); //-------------- > LAMBDA 
+            Console.Write("First 'A': " + s1);
+        }
+        static bool Test(string s) {
+            return s[0] == 'A';
+        }
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    //-----> FIM
+}
