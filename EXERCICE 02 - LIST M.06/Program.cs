d568fe6a7ba03d8fdd74fcc604731fe5202257bb
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-05-02 14:57:31 -0300

    FINAL

--- EXERCICE 02 - LIST M.06/Program.cs
+++ EXERCICE 02 - LIST M.06/Program.cs
@@ -16,10 +16,40 @@ namespace EXERCICE_02___LIST_M._06 {
             foreach (string obj in list) { //-------------- > PRINTA OS OBJETOS DA LISTA
                 Console.WriteLine(obj);
             }
-            Console.Write("list count: " + list.Count);
+                Console.Write("list count: " + list.Count);
+            string s1 = list.Find( x => x[0] =='A'); //-------------- > EXPRESSAO LAMBDA 
+                Console.Write("First 'A': " + s1);
+            string s2 = list.FindLast(x => x[0] == 'A'); //-------------- > EXPRESSAO LAMBDA 
+                Console.WriteLine("Last 'A': " +s2);
+            int pos1 = list.FindIndex(x => x[0] == 'A'); //-------------- > EXPRESSAO LAMBDA
+                Console.WriteLine("First position 'A': " + pos1);
+            int post2 = list.FindLastIndex(x => x[0] == 'A'); //-------------- > EXPRESSAO LAMBDA
+                Console.WriteLine("Last position 'A': " +post2);
 
-            string s1 = list.Find( x => x[0] =='A'); //-------------- > LAMBDA 
-            Console.Write("First 'A': " + s1);
+            List<string> list2 = list.FindAll(x => x.Length == 5);
+                foreach(string obj in list2) {
+                    Console.WriteLine(obj);
+                }
+            list.Remove("Alex");
+            Console.WriteLine("------------------");
+                foreach(string obj in list) {
+                    Console.WriteLine(obj);
+                }
+            list.RemoveAll(x => x[0] == 'M');
+            Console.WriteLine("------------------");
+                foreach(string obj in list) {
+                    Console.WriteLine(obj);
+                }
+            list.RemoveAt(3);
+            Console.WriteLine("------------------");
+                foreach(string obj in list) {
+                    Console.WriteLine(obj);
+                }
+            list.RemoveRange(2, 2);
+            Console.WriteLine("------------------");
+                foreach(string obj in list) {
+                    Console.WriteLine(obj);
+                }          
         }
         static bool Test(string s) {
             return s[0] == 'A';
